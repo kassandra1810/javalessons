bdb94ba5a38d320bea94139fde30cd8ff12eef55
kassandra1810 <liliabaluk@gmail.com>
2018-12-06 21:04:27 +0100

    Unit tests for Task 2 - empty class added

---
+++ src/test/java/javalessons/algorithms/frogjump/Task2Test.java
@@ -0,0 +1,14 @@
+package javalessons.algorithms.frogjump;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task2Test {
+
+    @Test
+    public void shouldPass(){
+        assertTrue("Should always pass",true);
+    }
+
+}
