bae77c59e3db048b8ce90867d333467da4e709b8
kassandra1810 <liliabaluk@gmail.com>
2018-12-08 18:12:47 +0100

    Unit tests for Task2 (FrogJump)

--- src/test/java/javalessons/algorithms/frogjump/Task2Test.java
+++ src/test/java/javalessons/algorithms/frogjump/Task2Test.java
@@ -7,8 +7,38 @@ import static org.junit.Assert.*;
 public class Task2Test {
 
     @Test
-    public void shouldPass(){
-        assertTrue("Should always pass",true);
+    public void shouldJumpThreeTimes() {
+        int inputX = 10;
+        int inputY = 40;
+        int inputD = 10;
+        int expectedResult = 3;
+
+        int actualResult = Task2.solution(inputX, inputY, inputD);
+
+        assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void shouldJumpZeroTimes() {
+        int inputX = 10;
+        int inputY = 10;
+        int inputD = 10;
+        int expectedResult = 0;
+
+        int actualResult = Task2.solution(inputX, inputY, inputD);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldStayInPlace() {
+        int inputX = 10;
+        int inputY = 1;
+        int inputD = 10;
+        int expectedResult = 0;
+
+        int actualResult = Task2.solution(inputX, inputY, inputD);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
